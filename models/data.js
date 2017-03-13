5c6ec69a623f9424ea0dff05d453fe0d09b5bbce
KellyUrango <johanaurango15@hotmail.com>
2017-03-12 22:07:45 -0500

    4 commit :D

--- models/data.js
+++ models/data.js
@@ -9,13 +9,13 @@ var datosElectrica = [
 ];
 
 var datosGasolina = [
-    {"nombre": "una", "precio": "11.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "Dos Dos", "precio": "12.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "Tres Tres Tres", "precio": "13.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "Cuatro Cuatro Cuatro", "precio": "14.000", "tipo": "deportivas", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "rrrrr", "precio": "15.000", "tipo": "turismo", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "eeeeee", "precio": "14.000", "tipo": "motocross", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "tttttt", "precio": "17.000", "tipo": "todot", "img": "http://localhost:3000/img/moto-03.jpg"}
+    {"nombre": "FZ 2.0", "precio": "6.490.000", "tipo": "urbana", "img": "http://localhost:3000/img/gasolina/urUna-01.jpg"},
+    {"nombre": "FAZER 2.0", "precio": "6.750.000", "tipo": "urbana", "img": "http://localhost:3000/img/gasolina/urDos-01.jpg"},
+    {"nombre": "SZ-RR", "precio": "4.990.000", "tipo": "urbana", "img": "http://localhost:3000/img/gasolina/urTres-01.jpg"},
+    {"nombre": "Cuatro Cuatro Cuatro", "precio": "14.000", "tipo": "deportivas", "img": "http://localhost:3000/img/gasolina/urUna-01.jpg"},
+    {"nombre": "rrrrr", "precio": "15.000", "tipo": "turismo", "img": "http://localhost:3000/img/gasolina/urUna-01.jpg"},
+    {"nombre": "eeeeee", "precio": "14.000", "tipo": "motocross", "img": "http://localhost:3000/img/gasolina/urUna-01.jpg"},
+    {"nombre": "tttttt", "precio": "17.000", "tipo": "todot", "img": "http://localhost:3000/img/gasolina/urUna-01.jpg"}
 ];
 
 exports.getAllGasolina = function () {
