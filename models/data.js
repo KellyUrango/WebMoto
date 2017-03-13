a5bbf597ebe6041ee1184291abf06e2a89a86695
KellyUrango <johanaurango15@hotmail.com>
2017-03-12 20:45:52 -0500

    3 commit :D

--- models/data.js
+++ models/data.js
@@ -9,13 +9,13 @@ var datosElectrica = [
 ];
 
 var datosGasolina = [
-    {"nombre": "unaf", "precio": "15.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "hola p", "precio": "15.000", "tipo": "urbana", "img": "http://localhost:3000|/img/moto-03.jpg"},
-    {"nombre": "aesoeso", "precio": "15.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "aeeeeedf", "precio": "15.000", "tipo": "deportivas", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "rrrrr", "precio": "12.000", "tipo": "turismo", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "eeeeee", "precio": "15.000", "tipo": "motocross", "img": "http://localhost:3000/img/moto-03.jpg"},
-    {"nombre": "tttttt", "precio": "15.000", "tipo": "todot", "img": "http://localhost:3000/img/moto-03.jpg"}
+    {"nombre": "una", "precio": "11.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "Dos Dos", "precio": "12.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "Tres Tres Tres", "precio": "13.000", "tipo": "urbana", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "Cuatro Cuatro Cuatro", "precio": "14.000", "tipo": "deportivas", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "rrrrr", "precio": "15.000", "tipo": "turismo", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "eeeeee", "precio": "14.000", "tipo": "motocross", "img": "http://localhost:3000/img/moto-03.jpg"},
+    {"nombre": "tttttt", "precio": "17.000", "tipo": "todot", "img": "http://localhost:3000/img/moto-03.jpg"}
 ];
 
 exports.getAllGasolina = function () {
